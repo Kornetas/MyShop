90dc6bb624b2b1a331272483016168d28d8487ae
Arek <arek93r@gmail.com>
2025-06-01 16:37:37 +0200

    Add Favorites list components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,12 @@ import "./styles/globals.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { MainPage } from "./views/MainPage/MainPage.jsx";
+import { Favorites } from "./components/Favourites/Favourites.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainPage />,
+    element: <Favorites />,
   },
 ]);
 
