741574328e185048876024bc747b1fd25435bb28
Arek <arek93r@gmail.com>
2025-06-03 18:05:45 +0200

    Fetch and display product list from backend

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,7 +7,9 @@ import { Cart } from "./views/Cart/Cart.jsx";
 import { Favourites } from "./views/Favourites/Favourites.jsx";
 import { Layout } from "./components/Layout/Layout.jsx";
 import { MainPage } from "./views/MainPage/MainPage.jsx";
+import { ProductsList } from "./views/ProductsList/ProductsList";
 import { mainPageLoader } from "./api/mainPageLoader.js";
+import { productListLoader } from "./api/productListLoader.js";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +28,11 @@ const router = createBrowserRouter([
         path: "/:gender?",
         element: <MainPage />,
         loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsList />,
+        loader: productListLoader,
       },
     ],
   },
